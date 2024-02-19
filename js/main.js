e7d35c1faeeaed47973e734829991d6f56498ebf
Muthaiahprojects <muthubu01@gmail.com>
2024-02-19 07:14:03 +0530

    op

--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@ const bar = document.querySelector(".bar")
 const loading = document.querySelectorAll(".loading");
 const span = document.querySelectorAll(".loading span");
 
+let op = document.querySelectorAll(".op")
+
+window.addEventListener("scroll",()=>{
+    for(let i=0;i<op.length;i++){
+        if(op[i].getBoundingClientRect().top <= 500){ 
+            op[i].classList.replace("op","bottomani")
+        }
+    }
+})
+
 window.addEventListener("scroll",()=>{
   if(bar.getBoundingClientRect().top <= 700){
     for(let i=0;i<loading.length;i++){
